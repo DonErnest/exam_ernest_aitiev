03223a039e3e8763c3600f8b478da3b89cc8f350
DonErnest <valeetmihifaveas@gmail.com>
2019-09-21 11:34:25 +0600

    Создание модели Record в models.py, ее регистрация в админ-панели admin.py, первая миграция

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Record
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'author_email','status','created_at']
+    list_display_links = ['id','author']
+    list_filter = ['author', 'status']
+    search_fields = ['text', 'author_email']
+    fields = ['author', 'author_email', 'text', 'status','created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Record, RecordAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+RECORD_STATUS = [('active', 'Активно'), ('blocked','Заблокировано')]
+
+
+class Record(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, verbose_name='Имя автора')
+    author_email = models.EmailField(null=False, blank=False, verbose_name='Почтовый адрес автора')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, verbose_name='Статус',default=RECORD_STATUS[0][0], choices=RECORD_STATUS)
+
+    def __str__(self):
+        return self.text
